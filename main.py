243d68e43bbada3737e3336336b071ec0eb9d58d
Andrew Siemer <andrew.siemer@eagles.oc.edu>
2021-03-29 20:24:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def record_temp():
 
     if db.query(Temperature).count() > 12:
         result = db.query(Temperature,func.min(Temperature.time))
-        print(result)
+        print(result[0][1])
         #db.delete(result)
         #db.commit()
 
