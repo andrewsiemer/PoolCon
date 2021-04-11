11c9835887ddcb1239e711f2144c42386fbb0ae8
Andrew Siemer <andrew.siemer@eagles.oc.edu>
2021-04-11 17:45:17 -0500

    Update chartjs.py

--- include/chartjs.py
+++ include/chartjs.py
@@ -21,7 +21,7 @@ class LineGraph(BaseChart):
             pointHoverBorderColor = "rgba(133, 135, 150, 1)"
             pointHitRadius = 10
             pointBorderWidth = 2
-            data = [10, 10]
+            data = []
         
     class options:
         maintainAspectRatio = False
