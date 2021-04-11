d7654fa9d66a0899baa1da14618135d018b0d5ce
Andrew Siemer <andrew.siemer@eagles.oc.edu>
2021-04-11 16:19:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def update_sensors():
     pool_data['pool-temp'] = str(water_temp.read()) + ' ºF'
     pool_data['water-level'] = str(water_level.read()) + ' %'
     pool_data['temp-chart'] = temp_chart.get()
-    pool_data['air-temp'] = air_temp.get_temp()
+    pool_data['air-temp'] = air_temp.read_temp() + ' ºF'
 
 @sched.scheduled_job('interval', start_date=str(datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)), hours=1)
 def record_temp():
