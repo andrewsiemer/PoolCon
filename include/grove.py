ff270ca7dcd2dce4e720487f2036a212c60d0da9
Andrew Siemer <andrew.siemer@eagles.oc.edu>
2021-04-11 18:02:07 -0500

    Update grove.py

--- include/grove.py
+++ include/grove.py
@@ -128,14 +128,12 @@ class Relay(object):
         if self.status == 'OFF':
             self.status = 'ON'
             relay_state |= (1 << (self.channel - 1))
-            write_i2c_block(relay_cmd + [self.addr, relay_state, unused])
-            read_i2c_block(no_bytes=1)
+            write_i2c_block(relay_cmd + [self.addr, relay_state, unused], self.i2c)
 
         else:
             self.status = 'OFF'
             relay_state &= ~(1 << (self.channel - 1))
-            write_i2c_block(relay_cmd + [self.addr, relay_state, unused])
-            read_i2c_block(no_bytes=1)
+            write_i2c_block(relay_cmd + [self.addr, relay_state, unused], self.i2c)
 
         return self.status
 
