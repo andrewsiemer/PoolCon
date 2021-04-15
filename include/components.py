84e9583dcff9a518aa64a1672eb479776c1b5720
Andrew Siemer <andrew.siemer@eagles.oc.edu>
2021-04-15 08:47:20 -0500

    Update components.py

--- include/components.py
+++ include/components.py
@@ -20,13 +20,14 @@ class Relay(object):
         if self.status == 'OFF':
             self.status = 'ON'
             print(grovepi.digitalRead(self.pin))
-            while grovepi.digitalRead(self.pin):  
+            while grovepi.digitalRead(self.pin) > 150:  
                 grovepi.digitalWrite(self.pin, 1)
-                time.sleep(0)
+                time.sleep(0.5)
         else:
             self.status = 'OFF'
-            while not grovepi.digitalRead(self.pin): 
+            while grovepi.digitalRead(self.pin) <= 150: 
                 grovepi.digitalWrite(self.pin, 0)
+                time.sleep(0.5)
 
         return self.status
 
