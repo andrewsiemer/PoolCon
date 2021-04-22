5f0768d6af829ac215cb6f2761fc8203573a6306
Andrew Siemer <andrew.siemer@eagles.oc.edu>
2021-04-21 22:40:59 -0500

    Update chartjs.py

--- include/chartjs.py
+++ include/chartjs.py
@@ -1,6 +1,90 @@
 from pychartjs import BaseChart, ChartType, Color, Options                                
 
-class LineGraph(BaseChart):
+class PumpGraph(BaseChart):
+
+    type = ChartType.Line
+
+    class labels:
+        grouped = []
+
+    class data:
+        class PumpUsage:
+            label = 'Pump Usage'
+            lineTension = 0.3
+            backgroundColor = "rgba(78, 115, 223, 0.05)"
+            borderColor = "rgba(78, 115, 223, 1)"
+            pointRadius = 3
+            pointBackgroundColor = "rgba(78, 115, 223, 1)"
+            pointBorderColor = "rgba(78, 115, 223, 1)"
+            pointHoverRadius = 3
+            pointHoverBackgroundColor = "rgba(78, 115, 223, 1)"
+            pointHoverBorderColor = "rgba(78, 115, 223, 1)"
+            pointHitRadius = 10
+            pointBorderWidth = 2
+            data = []
+        
+    class options:
+        maintainAspectRatio = False
+        
+        layout = {
+            'padding': {
+                'left': 10,
+                'right': 25,
+                'top': 0,
+                'bottom': 0
+            }
+        }
+
+        scales = {
+            'xAxes': [{
+                'time': {
+                'unit': 'hour'
+                },
+                'gridLines': {
+                'display': False,
+                'drawBorder': False
+                },
+                'ticks': {
+                'maxTicksLimit': 7
+                }
+            }],
+            'yAxes': [{
+                'ticks': {
+                'maxTicksLimit': 5,
+                'padding': 10
+                },
+                'gridLines': {
+                'color': "rgb(234, 236, 244)",
+                'zeroLineColor': "rgb(234, 236, 244)",
+                'drawBorder': False,
+                'borderDash': [2],
+                'zeroLineBorderDash': [2]
+                }
+            }],
+        }
+
+        legend = { 'display': True }
+
+        tooltips = {
+            'backgroundColor': "rgb(255,255,255)",
+            'bodyFontColor': "#858796",
+            'titleMarginBottom': 10,
+            'titleFontColor': '#6e707e',
+            'titleFontSize': 14,
+            'borderColor': '#dddfeb',
+            'borderWidth': 1,
+            'xPadding': 15,
+            'yPadding': 15,
+            'displayColors': False,
+            'intersect': False,
+            'mode': 'index',
+            'caretPadding': 10,
+        }
+
+    class pluginOptions:
+        pass
+
+class TempGraph(BaseChart):
 
     type = ChartType.Line
 
@@ -34,21 +118,6 @@ class LineGraph(BaseChart):
             pointHoverRadius = 3
             pointHoverBackgroundColor = "rgba(133, 135, 150, 1)"
             pointHoverBorderColor = "rgba(133, 135, 150, 1)"
-            pointHitRadius = 10
-            pointBorderWidth = 2
-            data = []
-        
-        class PumpUsage:
-            label = 'Pump Usage'
-            lineTension = 0.3
-            backgroundColor = "rgba(78, 115, 223, 0.05)"
-            borderColor = "rgba(78, 115, 223, 1)"
-            pointRadius = 3
-            pointBackgroundColor = "rgba(78, 115, 223, 1)"
-            pointBorderColor = "rgba(78, 115, 223, 1)"
-            pointHoverRadius = 3
-            pointHoverBackgroundColor = "rgba(78, 115, 223, 1)"
-            pointHoverBorderColor = "rgba(78, 115, 223, 1)"
             pointHitRadius = 10
             pointBorderWidth = 2
             data = []
