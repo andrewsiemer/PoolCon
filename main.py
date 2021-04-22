3d1c3c4514521b3da102e625b73607a37dd51edc
Andrew Siemer <andrew.siemer@eagles.oc.edu>
2021-04-22 00:12:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def update_sensors():
     #pool_data['pump-chart'] = pump_chart.get()
     pool_data['temp-chart'] = temp_chart.get()
     pool_data['schedule-tbl'] = crud.get_schedule_table()
-    pool_data['pump-time'] = str(stopwatch.duration)
+    pool_data['pump-time'] = str(datetime.strptime(round(stopwatch.duration),'%S').strftime('%-I hr %M mins'))
 
 def update_schedule():
     global pool_data
