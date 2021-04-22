27411c805463471ce99daae61c0adc26d32d9638
Andrew Siemer <andrew.siemer@eagles.oc.edu>
2021-04-21 20:40:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ pool_data = {
     'orp-level': str(round(orp_sensor.read())) + ' mV',
     'pump-chart': '', #pump_chart.get(),
     'temp-chart': temp_chart.get(),
-    'tbl-data': '123',
+    'tbl-data': 'OFF',
     'schedule-opt': ''
 }
 
@@ -190,7 +190,7 @@ def update_sensors():
     pool_data['orp-level'] = str(round(orp_sensor.read())) + ' mV'
     #pool_data['pump-chart'] = pump_chart.get()
     pool_data['temp-chart'] = temp_chart.get()
-    pool_data['tbl-data'] = '123'#crud.get_schedule_table()
+    pool_data['tbl-data'] = 'OFF'#crud.get_schedule_table()
 
 def toggle_event(event: str):
     global pool_data, pool_pump
